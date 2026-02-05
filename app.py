264d5d4bd71bfd355bd7978ab0498f82faafbf7b
IC-LGpro <isaiah.creagh@prolineproductsinc.com>
2026-02-05 11:06:04 -0600

    updated the debug in app.py to be false

--- app.py
+++ app.py
@@ -93,4 +93,4 @@ def serve_output(filename):
     return send_from_directory("output", filename)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=False)
